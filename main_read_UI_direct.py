f8123a1f58329c6e5edea81ea9a4c9d7dd43f5bf
CY Hung <y2603578y@gmail.com>
2024-05-19 15:37:26 +0800

    add_ui_class

--- main_read_UI_direct.py
+++ main_read_UI_direct.py
@@ -3,20 +3,24 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import QFile, QIODevice
 
+
+class HBMainWindow:
+    def __init__(self) -> None:
+        ui_file_name = "gui-1.ui"
+        ui_file = QFile(ui_file_name)
+        if not ui_file.open(QIODevice.ReadOnly):
+            print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+            sys.exit(-1)
+        loader = QUiLoader()
+        self.ui = loader.load(ui_file)
+        ui_file.close()
+        if not self.ui:
+            print(loader.errorString())
+            sys.exit(-1)
+
+
 if __name__ == "__main__":
-
-    ui_file_name = "gui-1.ui"
-    ui_file = QFile(ui_file_name)
-    if not ui_file.open(QIODevice.ReadOnly):
-        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
-        sys.exit(-1)
-    loader = QUiLoader()
     app = QApplication(sys.argv)
-    window = loader.load(ui_file)
-    ui_file.close()
-    if not window:
-        print(loader.errorString())
-        sys.exit(-1)
-    window.show()
-
+    hb_window = HBMainWindow()
+    hb_window.show()
     sys.exit(app.exec())
